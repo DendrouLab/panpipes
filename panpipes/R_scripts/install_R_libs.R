cb60744533dce55f2922733dfc357f57d91a28e7
Charlotte Rich-Griffin <25774602+crichgriffin@users.noreply.github.com>
2023-09-06 20:43:06 +0100

    specify same packages as conda

--- panpipes/R_scripts/install_R_libs.R
+++ panpipes/R_scripts/install_R_libs.R
@@ -1,17 +1,16 @@
-chooseCRANmirror(ind=38)
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
 install.packages("systemfonts", dependencies = TRUE)
-BiocManager::install(c("ComplexHeatmap"))
-
-cran_packages <- c("foreign","clustree","remotes","Seurat")
+cran_packages <- c("optparse", "foreign","clustree","remotes", "table")
 for (pp in cran_packages){
-    message("============ Installing ", pp, " ============")
-    install.packages(pp)
+  message("============ Installing ", pp, " ============")
+  install.packages(pp)
 }
 
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(c("ComplexHeatmap", "ggraph", 
+    "tidyverse", "ggforce","Seurat"))
 
 
 remotes::install_github("grimbough/rhdf5")
-remotes::install_github("ilia-kats/MuData")
-remotes::install_github("pmbio/MuDataSeurat")
+# remotes::install_github("ilia-kats/MuData")
+# remotes::install_github("pmbio/MuDataSeurat")
