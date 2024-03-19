c59a861bb6f516aa02fe3dc8275a70b2ac273279
SarahOuologuem <sarah.ouologuem@gmx.de>
2024-03-19 15:11:27 +0000

    update+add log info

--- panpipes/python_scripts/run_preprocess_spatial.py
+++ panpipes/python_scripts/run_preprocess_spatial.py
@@ -78,7 +78,6 @@ parser.add_argument("--n_pcs",
 
 args, opt = parser.parse_known_args()
 
-L.info("running with args:")
 L.info(args)
 
 figdir = args.figdir
@@ -89,6 +88,7 @@ if not os.path.exists(figdir):
 sc.settings.figdir = figdir
 sc.set_figure_params(scanpy=True, fontsize=14, dpi=300, facecolor='white', figsize=(5,5))
 
+L.info("Reading in MuData from '%s'" % args.input_mudata)
 mdata = mu.read(args.input_mudata)
 spatial = mdata.mod['spatial']
 
@@ -100,31 +100,37 @@ sprefix = input_data[:match.start()]
 
 # check if raw data is available
 #maybe layer of raw data as parameter
+L.info("Checking if raw data is available")
 if X_is_raw(spatial):
+    L.info("Saving raw counts from .X to .layers['raw_counts']")
     spatial.layers['raw_counts'] = spatial.X.copy()
 elif "raw_counts" in spatial.layers :
-    L.info("raw_counts layer already exists")
+    L.info(".layers['raw_counts'] already exists and copying it to .X")
     spatial.X = spatial.layers['raw_counts'].copy()
 else:
-    L.error("X is not raw data and 'raw_counts' layer not found")
     sys.exit("X is not raw data and 'raw_counts' layer not found")
 
-
+L.info("Using raw counts for HVG selection & normalization")
 
 # Normalization + HVG selection based on flavour
 if args.norm_hvg_flavour == "squidpy":
     if args.squidpy_hvg_flavour == "seurat_v3":
+        L.info("Running HVG selection with flavor seurat_v3")
         sc.pp.highly_variable_genes(spatial, flavor="seurat_v3", n_top_genes=int(args.n_top_genes), subset=args.filter_by_hvg,
                                     batch_key=args.hvg_batch_key)
+        L.info("Log-normalizing data")
         sc.pp.normalize_total(spatial)
         sc.pp.log1p(spatial)
     else:
+        L.info("Log-normalizing data")
         sc.pp.normalize_total(spatial)
         sc.pp.log1p(spatial)
+        L.info("Running HVG selection with flavor %s" % args.squidpy_hvg_flavour)
         sc.pp.highly_variable_genes(spatial, flavor=args.squidpy_hvg_flavour,
                                     min_mean=float(args.min_mean),
                                     max_mean=float(args.max_mean),
                                     min_disp=float(args.min_disp), subset=args.filter_by_hvg, batch_key=args.hvg_batch_key)
+    L.info("Saving log-normalized counts to .layers['lognorm']")
     spatial.layers["lognorm"] = spatial.X.copy()
     # plot HVGs:
     sc.pl.highly_variable_genes(spatial, show=False, save="_genes_highlyvar" + "."+ sprefix+ ".png")
@@ -138,12 +144,13 @@ elif args.norm_hvg_flavour == "seurat":
         clip = np.Inf
     else:
         clip = float(args.clip)
-
+    L.info("Running Pearson Residuals HVG selection")
     sce.pp.highly_variable_genes(spatial, theta=float(args.theta), clip=clip, n_top_genes=int(args.n_top_genes),
                                  batch_key=args.hvg_batch_key, flavor='pearson_residuals',
                                  layer="raw_counts", subset=args.filter_by_hvg)
-    
+    L.info("Running Pearson Residuals normalization")
     sce.pp.normalize_pearson_residuals(spatial, theta=float(args.theta), clip=clip, layer="raw_counts")
+    L.info("Saving log-normalized counts to .layers['norm_pearson_resid']")
     spatial.layers["norm_pearson_resid"] = spatial.X.copy()
 else:
     # error or warning?
@@ -151,18 +158,21 @@ else:
 
 
 if "highly_variable" in spatial.var:
-    L.warning( "You have %s Highly Variable Features", np.sum(spatial.var.highly_variable))
+    L.info("You have %s Highly Variable Features", np.sum(spatial.var.highly_variable))
 
 
 
 #PCA
+L.info("Running PCA")
 sc.pp.pca(spatial, n_comps=int(args.n_pcs), svd_solver='arpack', random_state=0)
+L.info("Plotting PCA")
 sc.pl.pca(spatial, save = "_vars" + "."+ sprefix+".png")
 sc.pl.pca_variance_ratio(spatial, log=True, n_pcs=int(args.n_pcs), save= "."+ sprefix+".png")
 
 
         
 mdata.update()
+L.info("Saving updated MuData to '%s'" % args.output_mudata)
 mdata.write(args.output_mudata)
 
 L.info("Done")
